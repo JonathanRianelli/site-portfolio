6e0d49b94ffbc44f11b01dbf1192ff0bfa287f0c
Jonathan Rianelli <jonthanrdo@gmail.com>
2022-07-18 20:55:36 -0300

    update

--- src/componentes/Projetos.js
+++ src/componentes/Projetos.js
@@ -1,3 +1,9 @@
+import hotel from "../img/hotel.gif";
+import crud from "../img/crud.gif";
+import clima from "../img/clima.png";
+import pokedex from "../img/pokedex.gif";
+import calculadora from "../img/calculadora.gif";
+import landingPage from "../img/landing-page.gif";
 
 export default function Projetos() {
 
@@ -13,42 +19,42 @@ export default function Projetos() {
             <div id="container">
                 <div className="box">
                     <h3>Hotel</h3>
-                    <img src="/img/hotel.gif" alt="Hotel" />
+                    <img src={hotel} alt="Hotel" />
                     <p>Projeto criado por mim para demonstrar meus conhecimentos em Front End.</p>
                     <button onClick={() =>abrirUrl('https://jonathanrianelli.github.io/site-hotel-responsivo/')} id="hbtn">Acessar site</button>
                     <button onClick={() =>abrirUrl('https://github.com/JonathanRianelli/site-hotel-responsivo')}>Repositório</button>
                 </div>
                 <div className="box">
                     <h3>Crud</h3>
-                    <img src="/img/crud.gif" alt="crud" />
+                    <img src={crud} alt="crud" />
                     <p>Projeto que usa local storage para armazenar as informações dos clientes.</p>
                     <button onClick={() =>abrirUrl('https://jonathanrianelli.github.io/projeto-crud/')}>Acessar site</button>
                     <button onClick={() =>abrirUrl('https://github.com/JonathanRianelli/projeto-crud')}>Repositório</button>
                 </div>
                 <div className="box">
                     <h3>Previsão do Tempo</h3>
-                    <img src="/img/clima.png" alt="Previsão do tempo" />
+                    <img src={clima} alt="Previsão do tempo" />
                     <p>Ferramenta de previsão do tempo desenvolvida em React, utilizando a API OpenWeatherMap.</p>
                     <button onClick={() =>abrirUrl('https://jonathanrianelli.github.io/previsao-do-tempo/')}>Acessar site</button>
                     <button onClick={() =>abrirUrl('https://github.com/JonathanRianelli/previsao-do-tempo')}>Repositório</button>
                 </div>
                 <div className="box">
                     <h3>Pokédex</h3>
-                    <img src="/img/pokedex.gif" alt="Pokédex" />
+                    <img src={pokedex} alt="Pokédex" />
                     <p>Uma pokédex criada junto com Dev em Dobro.</p>
                     <button onClick={() =>abrirUrl('https://jonathanrianelli.github.io/pokedex/')}>Acessar site</button>
                     <button onClick={() =>abrirUrl('https://github.com/JonathanRianelli/pokedex')}>Repositório</button>
                 </div>
                 <div className="box">
                     <h3>calculadora</h3>
-                    <img src="/img/calculadora.gif" alt="calculadora" />
+                    <img src={calculadora} alt="calculadora" />
                     <p>Calculadora simples, criada com a intenção de praticar meu JavaScript.</p>
                     <button onClick={() =>abrirUrl('https://github.com/JonathanRianelli/calculadora-js')}>Acessar site</button>
                     <button onClick={() =>abrirUrl('https://jonathanrianelli.github.io/calculadora-js/')}>Repositório</button>
                 </div>
                 <div className="box">
                     <h3>Landing Page</h3>
-                    <img src="/img/landing-page.gif" alt="Landing Page" />
+                    <img src={landingPage} alt="Landing Page" />
                     <p>Uma Landing Page criada junto com Dev em Dobro.</p>
                     <button onClick={() =>abrirUrl('https://jonathanrianelli.github.io/projeto-landing-page/')}>Acessar site</button>
                     <button onClick={() =>abrirUrl('https://github.com/JonathanRianelli/projeto-landing-page')}>Repositório</button>
--- src/componentes/Sobre.js
+++ src/componentes/Sobre.js
@@ -1,9 +1,11 @@
+import perfil from "../img/perfil.png";
+import curriculo from "../arquivos/Curriculo.pdf";
 
 export default function Sobre() {
 
     return (
         <section id="sobre">
-            <div><img src="/img/perfil.png" alt="Minha foto" /></div>
+            <div><img src={perfil} alt="Minha foto" /></div>
 
             <div>
                 <h2>Jonathan Rianelli</h2>
@@ -15,7 +17,7 @@ export default function Sobre() {
                     Sempre tive familiaridade e interesse por tecnologia, e desde que conheci a programação, me descobri na profissão e estou me dedicando a aprender cada vez mais sobre tudo que engloba esse universo.
                     Comecei meus estudos no final do ano passado, e desde então já concluí vários projetos próprios, alguns dos quais você pode conferir mais abaixo na aba "Projetos".</p>
 
-                <div id="btnBox"><a href="/arquivos/Curriculo.pdf" download="Currículo Jonathan Rianelli de Oliveira" id="btn">Baixar Currículo</a></div>
+                <div id="btnBox"><a href={curriculo} download="Currículo Jonathan Rianelli de Oliveira" id="btn">Baixar Currículo</a></div>
             </div>
         </section>
     )
