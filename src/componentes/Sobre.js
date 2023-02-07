ff1892f1a16244742b89a6821d1d0beec0127d54
JonathanHK <jonthanrdo@gmail.com>
2023-02-07 00:45:52 -0300

    update

--- src/componentes/Sobre.js
+++ src/componentes/Sobre.js
@@ -16,7 +16,7 @@ export default function Sobre() {
                 <p>Olá, seja bem vindo ao meu portfólio!
                     Eu sou o Jonathan, um estudante autodidata de programação.
                     Sempre tive familiaridade e interesse por tecnologia, e desde que conheci a programação, me descobri na profissão e estou me dedicando a aprender cada vez mais sobre tudo que engloba esse universo.
-                    Comecei meus estudos no final do ano passado, e desde então já concluí vários projetos próprios, alguns dos quais você pode conferir mais abaixo na aba "Projetos".</p>
+                    Comecei meus estudos no final de 2021, e desde então estudo diariamente e já concluí vários projetos próprios, alguns dos quais você pode conferir mais abaixo na aba "Projetos".</p>
 
                 <div id="btnBox"><a href={curriculo} download="Currículo Jonathan Rianelli de Oliveira" id="btn">Baixar Currículo</a></div>
             </div>
