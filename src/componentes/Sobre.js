4b78df48d08b121b01e60166bea6695ec51ac730
Jonathan Rianelli <jonthanrdo@gmail.com>
2025-03-04 19:43:49 -0300

    Update

--- src/componentes/Sobre.js
+++ src/componentes/Sobre.js
@@ -1,5 +1,5 @@
 import perfil from "../img/perfil.webp";
-import curriculo from "../arquivos/Currículo Jonathan Rianelli.pdf";
+import curriculo from "../arquivos/curriculo.pdf";
 import "../styles/sobre.scss";
 
 export default function Sobre() {
@@ -13,7 +13,7 @@ export default function Sobre() {
 
                 <h3>Desenvolvedor Web</h3>
 
-                <p>Olá, bem-vindo ao meu portfólio! Meu nome é Jonathan, e sempre fui apaixonado por tecnologia. Em 2018, trabalhei na manutenção de smartphones, porém, foi somente após descobrir a programação que encontrei minha verdadeira vocação. Em 2021 iniciei meus estudos, dedicando-me a aprender diversas linguagens e desenvolvendo vários projetos. Em janeiro de 2024, decidi dar um passo além, ingressando na faculdade de Análise e Desenvolvimento de Sistemas. Ao longo desses anos concluí diversos projetos pessoais, alguns dos quais você pode explorar mais abaixo, na seção 'Projetos'.</p>
+                <p>Olá, bem-vindo ao meu portfólio! Meu nome é Jonathan, e sempre fui apaixonado por tecnologia. Em 2018, trabalhei na manutenção de smartphones, mas foi na programação que encontrei minha verdadeira vocação. Em 2021, iniciei meus estudos e comecei a desenvolver projetos, explorando diversas tecnologias. Em janeiro de 2024, ingressei na faculdade de Análise e Desenvolvimento de Sistemas para aprofundar meus conhecimentos. Além dos estudos, trabalho como freelancer, aplicando minhas habilidades em projetos reais. Você pode conferir alguns dos meus projetos pessoais abaixo, na seção 'Projetos'.</p>
 
                 <div id="btnBox"><a href={curriculo} download="Currículo Jonathan Rianelli de Oliveira" id="btn">Baixar Currículo</a></div>
             </div>
