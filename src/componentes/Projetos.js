8af06f9229eb8103c2542861094a3c6e5cd48a9a
Jonathan Rianelli <jonthanrdo@gmail.com>
2026-07-28 19:44:50 -0300

    Páginação no Projetos mobile

--- src/componentes/Projetos.js
+++ src/componentes/Projetos.js
@@ -1,15 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ScrollReveal from "scrollreveal";
 import { FiExternalLink, FiLock, FiCode } from "react-icons/fi";
 import { BsStars } from "react-icons/bs";
 import { HiOutlineCube } from "react-icons/hi";
 import { IoRocketOutline } from "react-icons/io5";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
 import vzCell from "../img/projetos/vz-cell.gif";
 import grupoproject from "../img/projetos/grupoproject.gif";
 import n8nLogo from "../img/n8n-color.svg";
 import "../styles/projetos.scss";
 
 export default function Projetos() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 900);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth <= 900);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   useEffect(() => {
     const scrollReveal = ScrollReveal({
       distance: "30px",
@@ -111,67 +123,89 @@ export default function Projetos() {
     },
   ];
 
+  const renderCard = (projeto) => (
+    <div className="projeto-card">
+      {/* Imagem ou Placeholder */}
+      <div className="img-box">
+        {projeto.imagem ? (
+          <img src={projeto.imagem} alt={projeto.alt} />
+        ) : (
+          <div className="img-placeholder">
+            <span className="placeholder-icon">
+              {projeto.placeholderIcon}
+            </span>
+          </div>
+        )}
+
+        {/* Overlay de tecnologias no hover */}
+        <div className="tech-overlay">
+          {projeto.tecnologias.map((tech, i) => (
+            <span className="tech-icon" key={i} style={{ '--i': i }}>
+              {tech.tipo === "imagem" ? (
+                <span className="tech-img-wrapper">
+                  <img src={tech.src} alt={tech.alt} className="tech-img" />
+                </span>
+              ) : (
+                <i className={tech.icone}></i>
+              )}
+            </span>
+          ))}
+        </div>
+      </div>
+
+      {/* Conteúdo */}
+      <div className="card-content">
+        <h3>{projeto.titulo}</h3>
+        <p>{projeto.descricao}</p>
+      </div>
+
+      {/* Botões */}
+      <div className="botoes">
+        {projeto.tipo === "corporativo" ? (
+          <button className="botao-sozinho btn-corporativo" disabled>
+            <FiLock className="btn-icon" />
+            Projeto Corporativo
+          </button>
+        ) : (
+          <button
+            className="botao-sozinho"
+            onClick={() => abrirUrl(projeto.url)}
+          >
+            Acessar site
+            <FiExternalLink className="btn-icon" />
+          </button>
+        )}
+      </div>
+    </div>
+  );
+
   return (
     <section id="projetos">
       <h2 className="fade-up">Projetos</h2>
 
-      <div className="projetos-grid fade-up">
-        {projetos.map((projeto, index) => (
-          <div className="projeto-card" key={index}>
-            {/* Imagem ou Placeholder */}
-            <div className="img-box">
-              {projeto.imagem ? (
-                <img src={projeto.imagem} alt={projeto.alt} />
-              ) : (
-                <div className="img-placeholder">
-                  <span className="placeholder-icon">
-                    {projeto.placeholderIcon}
-                  </span>
-                </div>
-              )}
-
-              {/* Overlay de tecnologias no hover */}
-              <div className="tech-overlay">
-                {projeto.tecnologias.map((tech, i) => (
-                  <span className="tech-icon" key={i} style={{ '--i': i }}>
-                    {tech.tipo === "imagem" ? (
-                      <span className="tech-img-wrapper">
-                        <img src={tech.src} alt={tech.alt} className="tech-img" />
-                      </span>
-                    ) : (
-                      <i className={tech.icone}></i>
-                    )}
-                  </span>
-                ))}
-              </div>
-            </div>
-
-            {/* Conteúdo */}
-            <div className="card-content">
-              <h3>{projeto.titulo}</h3>
-              <p>{projeto.descricao}</p>
-            </div>
-
-            {/* Botões */}
-            <div className="botoes">
-              {projeto.tipo === "corporativo" ? (
-                <button className="botao-sozinho btn-corporativo" disabled>
-                  <FiLock className="btn-icon" />
-                  Projeto Corporativo
-                </button>
-              ) : (
-                <button
-                  className="botao-sozinho"
-                  onClick={() => abrirUrl(projeto.url)}
-                >
-                  Acessar site
-                  <FiExternalLink className="btn-icon" />
-                </button>
-              )}
-            </div>
-          </div>
-        ))}
-      </div>
+      {isMobile ? (
+        <Swiper
+          className="projetos-swiper fade-up"
+          modules={[Pagination]}
+          spaceBetween={16}
+          slidesPerView="auto"
+          centeredSlides
+          pagination={{ clickable: true }}
+          grabCursor
+        >
+          {projetos.map((projeto, index) => (
+            <SwiperSlide key={index} className="projeto-slide">
+              {renderCard(projeto)}
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      ) : (
+        <div className="projetos-grid fade-up">
+          {projetos.map((projeto, index) => (
+            <div key={index}>{renderCard(projeto)}</div>
+          ))}
+        </div>
+      )}
     </section>
   );
 }
